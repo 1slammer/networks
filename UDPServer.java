6ed68ba464c247cf8cc5427217fa4ad883d254ce
Wayne <jzn0016@auburn.edu>
2015-09-08 14:57:40 -0500

    It compiles now. Still haven't run it.

--- UDPServer.java
+++ UDPServer.java
@@ -6,11 +6,12 @@ import java.util.*;
 class UDPServer {
 
 public static void main(String args[]) throws Exception  { 
+  DatagramSocket serverSocket;
   if (args.length > 0) {
-     DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]);
+     serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
          }
   else {
-      DatagramSocket serverSocket = new DatagramSocket(10019);
+      serverSocket = new DatagramSocket(10019);
   }                     
   byte[] receiveData = new byte[256];                          
   while(true) {                   
@@ -21,12 +22,16 @@ public static void main(String args[]) throws Exception  {
       int length = data[0];
       byte request = data[1];
       int operation = data[2];
+      InetAddress IPAddress = receivePacket.getAddress();
+      int port = receivePacket.getPort();
+      byte[] sendData;
+      DatagramPacket sendPacket;
 
       switch (operation) {
         case 5: 
           int numConsonants = 0;
           for(int x = 3; x < data.length; x++) {
-            char ch = data[x];
+            char ch = (char) data[x];
             if (ch == 'a' || ch == 'e' || ch == 'i' || 
                                         ch == 'o' || ch == 'u')
                         {
@@ -37,20 +42,18 @@ public static void main(String args[]) throws Exception  {
                             numConsonants++;
                         }
           }
-          InetAddress IPAddress = receivePacket.getAddress();                   
-          int port = receivePacket.getPort();
 
-          byte[] sendData = new byte[3];
+          sendData = new byte[3];
           sendData[0] = 0x03;
           sendData[1] = request;
           sendData[2] = (byte) numConsonants;                   
-          DatagramPacket sendPacket =  new DatagramPacket(sendData, sendData.length, IPAddress, port);                   
+          sendPacket =  new DatagramPacket(sendData, sendData.length, IPAddress, port);                   
           serverSocket.send(sendPacket);                
           break;
         case 80:
           StringBuilder myBuilder = new StringBuilder();
           for(int x = 3; x < data.length; x++) {
-            char ch = data[x];
+            char ch = (char) data[x];
             
             if (ch == 'a' || ch == 'e' || ch == 'i' || 
                                         ch == 'o' || ch == 'u')
@@ -63,31 +66,27 @@ public static void main(String args[]) throws Exception  {
                         }
           }
           byte[] newBytes = myBuilder.toString().getBytes();
-          InetAddress IPAddress = receivePacket.getAddress();                   
-          int port = receivePacket.getPort();
 
-          byte[] sendData = new byte[newBytes.length + 2];
+          sendData = new byte[newBytes.length + 2];
           sendData[0] = 0x50;
           sendData[1] = request;
           for(int x = 0; x < sendData.length; x++){
             sendData[x + 2] = newBytes[x];
           }                   
-          DatagramPacket sendPacket =  new DatagramPacket(sendData, sendData.length, IPAddress, port);                   
+          sendPacket =  new DatagramPacket(sendData, sendData.length, IPAddress, port);                   
           serverSocket.send(sendPacket);                
           break;
         case 10:
           String sentence = new String( receivePacket.getData());                                     
-          InetAddress IPAddress = receivePacket.getAddress();                   
-          int port = receivePacket.getPort();                   
           String capitalizedSentence = sentence.toUpperCase();
           byte[] stringData = capitalizedSentence.getBytes();                   
-          byte[] sendData = new byte[stringData.length + 2];
+          sendData = new byte[stringData.length + 2];
           sendData[0] = 0x0A;
           sendData[1] = request;
           for(int x = 0; x < sendData.length; x++){
             sendData[x + 2] = stringData[x];
           }                                
-          DatagramPacket sendPacket =  new DatagramPacket(sendData, sendData.length, IPAddress, port);                   
+          sendPacket =  new DatagramPacket(sendData, sendData.length, IPAddress, port);                   
           serverSocket.send(sendPacket);                
       }                   
       }       
