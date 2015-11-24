f84c905b08184a4466bd1dc34d27b1c90e5cf9f8
Dillon A Beck <dab0021@tux195.eng.auburn.edu>
2015-11-24 17:02:53 -0600

    Add more skeleton code to TCPClient

--- p3/Client/TCPClient.py
+++ p3/Client/TCPClient.py
@@ -3,12 +3,13 @@ import socket
 
 class Client():
 
-	CHAT_INVALID = 0
-	CHAT_READY = 1
-	CHAT_WAITING = 2
+	RESPONSE_INVALID = 0
+	RESPONSE_READY = 1
+	RESPONSE_WAITING = 2
 
 	def __init__(self, serverName, serverPort, clientPort):
 		self.sock = None
+		self.clientPort = clientPort
 		self.setup(serverName, serverPort, clientPort)
 
 
@@ -26,12 +27,12 @@ class Client():
 		response = self.sendChatRequest(request)
 		state = self.parseResponse(response)
 
-		if state == self.CHAT_INVALID:
-			pass
-		elif state == self.CHAT_WAITING:
-			pass
-		elif state == self.CHAT_READY:
-			pass
+		if state == self.RESPONSE_INVALID:
+			self.displayInvalidResponseMessage(response)
+		elif state == self.RESPONSE_WAITING:
+			self.waitForChatPartner()
+		elif state == self.RESPONSE_READY:
+			self.connectToChatServer()
 		
 	def formRequest(self):
 		request = None
@@ -45,19 +46,56 @@ class Client():
 
 	def parseResponse(self, response):
 		# if invalid response
-		return self.CHAT_INVALID
+		return self.RESPONSE_INVALID
 
 		# if reponse only magic number
-		return self.CHAT_WAITING
+		return self.RESPONSE_WAITING
 
 		# if response complete
-		return self.CHAT_READY
+		return self.RESPONSE_READY
 
 
 
+	def displayInvalidResponseMessage(self, response):
+		print "Invalid response from server"
+		print response
+
+	def waitForChatPartner(self):
+		print "waiting for a partner to connect"
+		self.setUpChatServer()
+
+	def setUpChatServer(self):
+		# Create a TCP socket
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		# Bind the socket to the port
+		server_address = ('localhost', self.clientPort)
+		print "starting up on %s port %s" % server_address
+		self.sock.bind(server_address)
+
+		# Listen for incoming connections
+		sock.listen(1)
+
+		while True:
+			# Wait for a connection
+			print "Waiting for a connection"
+			connection, client_address = sock.accept()
+
+			try:
+				print "connection from %s" % client_address
+
+				# Recieve data in a single chunk
+				data = connection.recv(256)
+				print "received %s" % data
+
+			finally:
+				connection.close()
+
+
+	def connectToChatServer(self):
+		pass
 
 		
-  
 
 
 def main(serverName, serverPort, clientPort):
