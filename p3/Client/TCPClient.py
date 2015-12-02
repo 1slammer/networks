c020341c64d5c693b2be18d8ac0df77c9cdb492a
Guillermo- <githubemails@gmail.com>
2015-12-02 17:40:52 -0600

    nothing

--- p3/Client/TCPClient.py
+++ p3/Client/TCPClient.py
@@ -107,9 +107,6 @@ class Client(object):
 	def requestChat(self):
 		request = self.formRequest()
 		response = self.sendChatRequest(request)
-		print
-		print response
-		print
 		state = self.parseResponse(response)
 
 		if state == self.RESPONSE_INVALID:
